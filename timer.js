a7736e0b7bfbfd4b88625fda657a122075426554
Aneesh <katta.aneesh597@gmail.com>
2023-01-14 15:00:23 +0530

    added program

--- timer.js
+++ timer.js
@@ -1,10 +1,48 @@
-function showtime(){
-    const date =new Date();
-    return date.getHours()+":hrs"+date.getMinutes()+":min"+date.getSeconds()+":secs";
+let XmlHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+function showtime() {
+    const date = new Date();
+    return date.getHours() + ":hrs" + date.getMinutes() + ":min" + date.getSeconds() + ":secs";
 }
-function showSessionExpire(){
-console.log("Activity B expired at :"+showtime());
+function makeAJAXCall(methodType,url,callback,async = true,data = null) {
+    let xhr = new XmlHttpRequest();
+    xhr.onreadystatechange = function () {
+        console.log("state change called ready state" + xhr.readyState + "at :" + showtime() + " status" + xhr.status);
+    
+    if (xhr.status === 4) {
+        if (xhr.status === 200 || xhr.status === 201) {
+            callback(xhr.responseText);
+        }
+        else if (xhr.status >= 400) {
+            console.log("handle 400 client error" + xhr.status + "at:" + showtime());
+        }
+    }
 }
-console.log("Activity A triggering activity B at :"+showtime());
-setTimeout(showSessionExpire,5000);
-console.log("Activity A triggering activity B at :"+showtime()+" will executed after 5 secs");
+xhr.open(methodType, url, async);
+if (data) {
+    console.log(JSON.stringify(data));
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(data));
+}
+else
+xhr.send();
+console.log(methodType + " request sent to server at" + showtime());     
+}
+const getURL="http://localhost:3000/employees/";
+function getUserDetails(data) {
+    console.log("get user data"+data);
+}
+makeAJAXCall("GET",getURL,getUserDetails);
+// console.log("made GET AJAX Call at "+showtime());
+// const deleteURL = "http://localhost:3000/employees/6";
+// function userDeleted(data){
+//     console.log(" user deleted "+data);
+// }
+// makeAJAXCall("DELETE",deleteURL,userDeleted,true);
+console.log("made DELETE AJAX Call at "+showtime());
+const postURL="http://localhost:3000/employees/";
+const empData={"id":"06","name":"kiran","salary":"50000"};
+function userAdded(data) {
+    console.log("user data:"+data);
+}
+makeAJAXCall("POST",postURL,userAdded,true,empData);
+console.log("made POST AJAX Call at "+showtime());

fdaa5b1e2007e065a397bf460496dc184297049e
Aneesh <katta.aneesh597@gmail.com>
2023-01-15 23:24:27 +0530

    added  program for promisecall get ,delete ,post operations

--- timer.js
+++ timer.js
@@ -1,48 +1,54 @@
-let XmlHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 function showtime() {
     const date = new Date();
     return date.getHours() + ":hrs" + date.getMinutes() + ":min" + date.getSeconds() + ":secs";
 }
-function makeAJAXCall(methodType,url,callback,async = true,data = null) {
-    let xhr = new XmlHttpRequest();
-    xhr.onreadystatechange = function () {
-        console.log("state change called ready state" + xhr.readyState + "at :" + showtime() + " status" + xhr.status);
-    
-    if (xhr.status === 4) {
-        if (xhr.status === 200 || xhr.status === 201) {
-            callback(xhr.responseText);
+function makePromiseCall(methodType, url, async = true, data = null) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            console.log("state change called ready state" + xhr.readyState + "at :" + showtime() + " status" + xhr.status);
+            if (xhr.status.toString().match('^[2][0-9]{2}$')) 
+                resolve(xhr.responseText);
+            
+            else if (xhr.status.toString().match('^[4,5][0-9]{2}$')) {
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText
+                });
+                console.log("xhr failed");
+            }
         }
-        else if (xhr.status >= 400) {
-            console.log("handle 400 client error" + xhr.status + "at:" + showtime());
-        }
+            xhr.open(methodType, url, async);
+            if (data) {
+                console.log(JSON.stringify(data));
+                xhr.setRequestHeader("Content-Type", "application/json");
+                xhr.send(JSON.stringify(data));
+            }
+            else xhr.send();
+            console.log(methodType + " request sent to server at" + showtime());
+    });   
+}
+    const getURL = "http://localhost:3000/employees/";
+    makePromiseCall("GET", getURL, true)
+    .then(responseText => {
+        console.log(" user added post "+responseText)
+    })
+    .catch(error => console.log("get error status "+JSON.stringify(error)));
+
+    const deleteURL = "http://localhost:3000/employees/6";
+    makePromiseCall("DELETE", deleteURL, false)
+    .then(responseText => {
+        console.log(" user deleted post "+responseText)
     }
-}
-xhr.open(methodType, url, async);
-if (data) {
-    console.log(JSON.stringify(data));
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify(data));
-}
-else
-xhr.send();
-console.log(methodType + " request sent to server at" + showtime());     
-}
-const getURL="http://localhost:3000/employees/";
-function getUserDetails(data) {
-    console.log("get user data"+data);
-}
-makeAJAXCall("GET",getURL,getUserDetails);
-// console.log("made GET AJAX Call at "+showtime());
-// const deleteURL = "http://localhost:3000/employees/6";
-// function userDeleted(data){
-//     console.log(" user deleted "+data);
-// }
-// makeAJAXCall("DELETE",deleteURL,userDeleted,true);
-console.log("made DELETE AJAX Call at "+showtime());
-const postURL="http://localhost:3000/employees/";
-const empData={"id":"06","name":"kiran","salary":"50000"};
-function userAdded(data) {
-    console.log("user data:"+data);
-}
-makeAJAXCall("POST",postURL,userAdded,true,empData);
-console.log("made POST AJAX Call at "+showtime());
+    )
+    .catch(error => console.log("delete error status "+JSON.stringify(error)));
+    const postURL = "http://localhost:3000/employees/";
+    const empData = { "name": "ramu", "salary": "50000" }
+    makePromiseCall("POST", postURL, true, empData)
+    .then(responseText => {
+        console.log(" user added post "+responseText)
+    })
+    .catch(error => console.log("post error status "+JSON.stringify(error)));
+
+

5307ff2ab4395889e4f61e42bfc7f9dfe0e296c5
Aneesh <katta.aneesh597@gmail.com>
2023-01-16 07:55:44 +0530

    added program

--- timer.js
+++ timer.js
@@ -40,33 +40,12 @@ function makePromiseCall(methodType, url, async = true, data = null) {
     .then(responseText => {
         console.log(" user deleted post "+responseText)
     }
-}
-xhr.open(methodType, url, async);
-if (data) {
-    console.log(JSON.stringify(data));
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.send(JSON.stringify(data));
-}
-else
-xhr.send();
-console.log(methodType + " request sent to server at" + showtime());     
-}
-const getURL="http://localhost:3000/employees/";
-function getUserDetails(data) {
-    console.log("get user data"+data);
-}
-makeAJAXCall("GET",getURL,getUserDetails);
-console.log("made GET AJAX Call at "+showtime());
-const deleteURL = "http://localhost:3000/employees/8";
-function userDeleted(data){
-    console.log(" user deleted "+data);
-}
-makeAJAXCall("DELETE",deleteURL,userDeleted,true);
-console.log("made DELETE AJAX Call at "+showtime());
-const postURL="http://localhost:3000/employees/";
-const empData={"name":"kiran","salary":"50000"};
-function userAdded(data) {
-    console.log("user data:"+data);
-}
-makeAJAXCall("POST",postURL,userAdded,true,empData);
-console.log("made POST AJAX Call at "+showtime());
+    )
+    .catch(error => console.log("delete error status "+JSON.stringify(error)));
+    const postURL = "http://localhost:3000/employees/";
+    const empData = { "name": "ramu", "salary": "50000" }
+    makePromiseCall("POST", postURL, true, empData)
+    .then(responseText => {
+        console.log(" user added post "+responseText)
+    })
+    .catch(error => console.log("post error status "+JSON.stringify(error)));

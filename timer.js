fb58d236f2b781e4c96465d66c9f15a6b78445b9
Aneesh <katta.aneesh597@gmail.com>
2023-01-16 07:53:52 +0530

    added program

--- timer.js
+++ timer.js
@@ -3,17 +3,42 @@ function showtime() {
     const date = new Date();
     return date.getHours() + ":hrs" + date.getMinutes() + ":min" + date.getSeconds() + ":secs";
 }
-function makeAJAXCall(methodType,url,callback,async = true,data = null) {
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        console.log("state change called ready state" + xhr.readyState + "at :" + showtime() + " status" + xhr.status);
-    if (xhr.readyState === 4) {
-        if (xhr.status === 200 || xhr.status === 201) {
-            callback(xhr.responseText);
+function makePromiseCall(methodType, url, async = true, data = null) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            console.log("state change called ready state" + xhr.readyState + "at :" + showtime() + " status" + xhr.status);
+            if (xhr.status.toString().match('^[2][0-9]{2}$')) 
+                resolve(xhr.responseText);
+            
+            else if (xhr.status.toString().match('^[4,5][0-9]{2}$')) {
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText
+                });
+                console.log("xhr failed");
+            }
         }
-        else if (xhr.status >= 400) {
-            console.log("handle 400 client error" + xhr.status + "at:" + showtime());
-        }
+            xhr.open(methodType, url, async);
+            if (data) {
+                console.log(JSON.stringify(data));
+                xhr.setRequestHeader("Content-Type", "application/json");
+                xhr.send(JSON.stringify(data));
+            }
+            else xhr.send();
+            console.log(methodType + " request sent to server at" + showtime());
+    });   
+}
+    const getURL = "http://localhost:3000/employees/";
+    makePromiseCall("GET", getURL, true)
+    .then(responseText => {
+        console.log(" user added post "+responseText)
+    })
+    .catch(error => console.log("get error status "+JSON.stringify(error)));
+    const deleteURL = "http://localhost:3000/employees/6";
+    makePromiseCall("DELETE", deleteURL, false)
+    .then(responseText => {
+        console.log(" user deleted post "+responseText)
     }
 }
 xhr.open(methodType, url, async);
